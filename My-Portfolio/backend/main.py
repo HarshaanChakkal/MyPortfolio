da7806c4ad526c72dd6352d3955cb1189f20f144
HarshaanChakkal <h895c959@ku.edu>
2025-08-08 01:17:42 -0500

    backend

--- My-Portfolio/backend/main.py
+++ My-Portfolio/backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["portfolio-harshaans-projects.vercel.app"],
+    allow_origins=["https://portfolio-harshaans-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
